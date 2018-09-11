03241d70a3754edf0406f81473b76458f5feabec
amandademetrio <amandademetrio@gmail.com>
2018-09-10 22:42:57 -0700

    first commit

--- RainbowRow/ViewController.swift
+++ RainbowRow/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    class RainbowRow {
+        let color: UIColor
+        let height: Int = 120
+        init(color: UIColor) {
+            self.color = color
+        }
+        
+        var red = UIColor.red
+    }
+    
+    var rainbowRowArray: [RainbowRow] = [RainbowRow(color:UIColor.red),RainbowRow(color:UIColor.orange),RainbowRow(color:UIColor.yellow),RainbowRow(color:UIColor.green),RainbowRow(color:UIColor.blue),RainbowRow(color:UIColor.purple)]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 120.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rainbowRowArray.count
+    }
+    
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = rainbowRowArray[indexPath.row].color
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+    }
+    
+}
